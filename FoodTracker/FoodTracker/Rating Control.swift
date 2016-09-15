3050d53f5269707a79fa64b1f0a0ee5c9a7a3215
Danny Nguyen <dannynguyen@Dannys-MacBook-Pro.local>
2016-09-15 16:34:58 -0500

    Add new source.

---
+++ FoodTracker/FoodTracker/Rating Control.swift
@@ -0,0 +1,39 @@
+//
+//  Rating Control.swift
+//  FoodTracker
+//
+//  Created by Danny Nguyen on 9/15/16.
+//  Copyright © 2016 Danny Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class Rating_Control: UIView {
+    
+    //////
+    
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+
+  // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.redColor()
+        button.addTarget(self, action: #selector(Rating_Control.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+        addSubview(button)
+    }
+    
+    // MARK: Button Action
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+}
